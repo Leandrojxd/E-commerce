ac3972666dac8af11863e0e7fe1123d0c58c5c78
Leandro Juarez <leandrojuarez655@gmail.com>
2023-11-22 22:36:23 -0400

    feature: Complete Template View of EmptyShoppingCart

--- src/components/templates/EmptyShoppingCart.tsx
+++ src/components/templates/EmptyShoppingCart.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import CustomButton from '../atoms/CustomButton'
 import styles from 'public/home.module.css'
+import CustomText from '../atoms/CustomText'
 
 function EmptyShoppingCart() {
   return (
-    <div>
-        <CustomButton content={'LLEVAME A LOS PRODUCTOS'} styleButton={styles.button_shopping_cart_empty_style} styleText={styles.back_button_text_style} />
+    <div className={styles.empty_shopping_cart_position}>
+        <CustomText content='Aun no has agregado nada a tu carrito.' customClassName={styles.empty_message_text_style}/>
+        <CustomButton 
+        content={'LLEVAME A LOS PRODUCTOS'} 
+        styleButton={styles.button_shopping_cart_empty_style}
+         styleText={styles.back_button_text_style} />
+
     </div>
   )
 }
