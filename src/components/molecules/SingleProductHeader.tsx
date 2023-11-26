f03ba9aada39d9618dde25a1ff3bfdf826aced1c
Leandro Juarez <leandrojuarez655@gmail.com>
2023-11-25 23:58:37 -0400

    test: Commit Not Working Test

--- src/components/molecules/SingleProductHeader.tsx
+++ src/components/molecules/SingleProductHeader.tsx
@@ -8,7 +8,7 @@ function SingleProductHeader() {
             <h1 className={styles.title_card}>CILINDRO DE ALUMINIO DE 1M3</h1>
             <p className={styles.brand_card}>INEGAR</p>
         </div>
-        <div className=''>
+        <div className={}>
             <h1 className={styles.price_card}>$us 25.00</h1>
             <p className={styles.description_card}>caja de 12 pzas.</p>
         </div>
