5268b38c34db00cb7ca8920265c8e43cee0ac558
Leandro Juarez <leandrojuarez655@gmail.com>
2023-11-26 23:30:54 -0400

    feature: Editing Style position for Single Product Header

--- src/components/molecules/SingleProductHeader.tsx
+++ src/components/molecules/SingleProductHeader.tsx
@@ -4,14 +4,16 @@ import styles from 'public/home.module.css'
 function SingleProductHeader() {
   return (
     <div>
-        <div className=''>
+      <div className={styles.single_product_header_position}>
+       <div>
             <h1 className={styles.title_card}>CILINDRO DE ALUMINIO DE 1M3</h1>
             <p className={styles.brand_card}>INEGAR</p>
         </div>
-        <div className={}>
+        <div >
             <h1 className={styles.price_card}>$us 25.00</h1>
             <p className={styles.description_card}>caja de 12 pzas.</p>
         </div>
+      </div>
     </div>
   )
 }
