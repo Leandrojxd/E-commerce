2bb9dc652cefee6150f5df4f5cfc0b85c64107fa
Leandro Juarez <leandrojuarez655@gmail.com>
2023-12-02 17:36:41 -0400

    fix: Centergin a div in Button Back to Products

--- src/app/ShoppingCart/page.tsx
+++ src/app/ShoppingCart/page.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 export default function ShoppingCart() {
   return (
     <div>
-        <TopBarHeader/>
         <EmptyShoppingCart />
     </div>
   )
--- src/components/templates/EmptyShoppingCart.tsx
+++ src/components/templates/EmptyShoppingCart.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import CustomButton from '../atoms/CustomButton'
 import styles from 'public/home.module.css'
 import CustomText from '../atoms/CustomText'
+import TopBarHeader from '../organism/TopBarHeader'
 
 function EmptyShoppingCart() {
   return (
-    <div className={styles.empty_shopping_cart_position}>
-        <CustomText content='Aun no has agregado nada a tu carrito.' customClassName={styles.empty_message_text_style}/>
-        <CustomButton 
-        content={'LLEVAME A LOS PRODUCTOS'} 
-        styleButton={styles.button_shopping_cart_empty_style}
-         styleText={styles.back_button_text_style} />
+    <>
+        <TopBarHeader/>
+        <div className={styles.empty_shopping_cart_position}>
+            <CustomText content='Aun no has agregado nada a tu carrito.' customClassName={styles.empty_message_text_style}/>
+            <CustomButton 
+            content={'LLEVAME A LOS PRODUCTOS'} 
+            styleButton={styles.button_shopping_cart_empty_style}
+            styleText={styles.back_button_text_style} 
+            itemButtonDisplay={styles.back_products_button_position}/>
 
-    </div>
+        </div>
+    </>
+
   )
 }
 
