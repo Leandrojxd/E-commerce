d3c82f16edfece3577e467faa9e2d78b389296af
Leandro Juarez <leandrojuarez655@gmail.com>
2023-12-15 23:48:57 -0400

    fix: Into Molecules

---
+++ src/components/molecules/ReserveProductInformation.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from 'public/home.module.css'
+
+function ReserveProductInformation() {
+  return (
+    <div>
+        <div>
+            <p className={styles.title_card}>CILINDRO DE ALUMINIO DE 1M3</p>
+        </div>
+          
+        <div className={styles.info_reserve_position}>
+            <p className={styles.brand_card}>INEGAR</p>
+            <p className={styles.description_card}>$us 25.00</p>
+        </div>
+    </div>
+  )
+}
+
+export default ReserveProductInformation
