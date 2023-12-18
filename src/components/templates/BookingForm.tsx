1dc3b73e9be40f3ad689e85700029dfef21b5cef
Leandro Juarez Candia <leandrojuarez655@gmail.com>
2023-12-17 23:22:04 -0400

    fix: Css Shopping Cart Form

--- src/components/templates/BookingForm.tsx
+++ src/components/templates/BookingForm.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import styles from 'public/home.module.css'
+import React from "react";
+import styles from "public/home.module.css";
 
 function BookingForm() {
   return (
     <div className={styles.booking_form_style}>
-        <h1 className={styles.title_style2}>Datos de tu reserva</h1>
-        <p className={styles.description_card}>
-            Rellena este formulario para que podamos contactarte por whatsapp.
-        </p>
-        <h2 className={styles.subtitle_style}>Nombre</h2>
-        <input className={styles.input_style} type="text" placeholder='Ingresa tu nombre'/>
-        <h2 className={styles.subtitle_style}>Ciudad Destino</h2>
-        <input className={styles.input_style} type="text" placeholder='Selecciona destino'/>
+      <h1 className={styles.title_form_style}>Datos de tu reserva</h1>
+      <p className={styles.description_form_style}>
+        Rellena este formulario para que podamos contactarte por whatsapp.
+      </p>
+      <h2 className={styles.subtitle_style}>Nombre</h2>
+      <input
+        className={styles.input_style}
+        type="text"
+        placeholder="Ingresa tu nombre"
+      />
+      <h2 className={styles.subtitle_style}>Ciudad Destino</h2>
+      <input
+        className={styles.input_style}
+        type="text"
+        placeholder="Selecciona destino"
+      />
     </div>
-  )
+  );
 }
 
-export default BookingForm
+export default BookingForm;
