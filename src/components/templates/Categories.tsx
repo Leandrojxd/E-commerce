f9ffb5671a53ce53b38fdfcd1e8a172881245ee6
Leandro Juarez Candia <leandrojuarez655@gmail.com>
2024-01-22 21:16:25 -0400

    fix: Adding other way of Loading Skeleton

--- src/components/templates/Categories.tsx
+++ src/components/templates/Categories.tsx
@@ -1,26 +1,48 @@
+"use client";
+
 import CategoryItem from "../molecules/CategoryItem";
 import styles from "public/home.module.css";
 import { getAllCategories } from "@/pages/api/Services";
 import { CategoriesData } from "@/pages/api/DataType";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect, useState } from "react";
 
-async function Categories() {
-  const categories: CategoriesData[] = await getAllCategories();
+function Categories() {
+  const [categories, setCategories] = useState<CategoriesData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const categoriesData = await getAllCategories();
+        setCategories(categoriesData);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.categories}>
       <h1 className={styles.title_style}>CATEGORIAS</h1>
 
-      {categories.map((category: CategoriesData, index: number) => (
-        <CategoryItem
-          key={index}
-          source={category.Url_Image}
-          title={category.Name}
-          subtitle={category.Description}
-        />
-      )) || <Skeleton height={30} width={30}/>}
-
+      {loading ? (
+        <Skeleton height={30} width={30} />
+      ) : (
+        categories.map((category, index) => (
+          <CategoryItem
+            key={index}
+            source={category.Url_Image}
+            title={category.Name}
+            subtitle={category.Description}
+          />
+        ))
+      )}
     </div>
   );
 }
