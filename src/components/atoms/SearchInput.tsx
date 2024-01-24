c625682b7cf6aad7cc6ff5af08290bcf00a50e48
Leandro Juarez Candia <leandrojuarez655@gmail.com>
2024-01-23 20:50:22 -0400

    feature: SearchQuery with useRouter and obtain the quer

--- src/components/atoms/SearchInput.tsx
+++ src/components/atoms/SearchInput.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import style from 'public/home.module.css'
+"use client";
+
+import React, { useState } from "react";
+import style from "public/home.module.css";
+import { useRouter } from "next/navigation";
 
 type SearchInputProps = {
   placeholder: string;
-}
+};
 
-function SearchInput({placeholder}: SearchInputProps) {
+function SearchInput({ placeholder }: SearchInputProps) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const onSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const encodedSearchQuery = encodeURI(searchQuery);
+    router.push(`/Catalog?q=${encodedSearchQuery}`)
+  }
+
   return (
     <div>
-        <input type="text" placeholder={placeholder} className={style.searchInput_style} />
+      <form onSubmit={onSearch}>
+        <input
+          value={searchQuery}
+          onChange={(event) => {
+            setSearchQuery(event.target.value);
+          }}
+          type="text"
+          placeholder={placeholder}
+          className={style.searchInput_style}
+        />
+      </form>
     </div>
-  )
+  );
 }
 
-export default SearchInput
+export default SearchInput;
