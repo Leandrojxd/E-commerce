d69bf928e8445e1a9348ecc77e862e308f885d30
Leandro Juarez Candia <leandrojuarez655@gmail.com>
2024-02-02 23:55:01 -0400

    fix: Agregate Button delete some lines of code

--- src/app/Catalog/[product_id]/page.tsx
+++ src/app/Catalog/[product_id]/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import SingleProduct from "@/components/organism/SingleProduct";
-import { SingleProductInfo } from "@/pages/api/DataType";
-import { serviceFetchSingleProductById } from "@/pages/api/Services";
-import { useEffect, useState } from "react";
 
 type ProductId = {
   product_id: string;
@@ -14,20 +11,6 @@ type ProductsProps = {
 };
 
 export default function Product({ params }: ProductsProps) {
-  const [singleProduct, setSingleProduct] = useState<SingleProductInfo>();
-  const [loading,setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchSingleProductById = async (Product_id: string) => {
-      const singleProductSupabase = await serviceFetchSingleProductById(
-        Product_id
-      );
-      setSingleProduct(singleProductSupabase as SingleProductInfo);
-      setLoading(false);
-    };
-    fetchSingleProductById(params.product_id);
-  }, [params.product_id]);
-
   return (
     <div>
       <SingleProduct product_id={params.product_id} />
