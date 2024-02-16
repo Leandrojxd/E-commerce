d533353935d5295baf37a8ea39191f14f678a1cf
Leandro Juarez Candia <leandrojuarez655@gmail.com>
2024-02-15 20:38:33 -0400

    feature: Functionality to the button LLEVAME A LOS PRODCUTS , in empty shopping cart

--- src/components/templates/EmptyShoppingCart.tsx
+++ src/components/templates/EmptyShoppingCart.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import CustomButton from '../atoms/CustomButton'
-import styles from 'public/home.module.css'
-import CustomText from '../atoms/CustomText'
-import TopBarHeader from '../organism/TopBarHeader'
+import React from "react";
+import CustomButton from "../atoms/CustomButton";
+import styles from "public/home.module.css";
+import CustomText from "../atoms/CustomText";
+import TopBarHeader from "../organism/TopBarHeader";
+import Link from "next/link";
 
 function EmptyShoppingCart() {
   return (
     <>
-        <TopBarHeader/>
-        <div className={styles.empty_shopping_cart_position}>
-            <CustomText content='Aun no has agregado nada a tu carrito.' customClassName={styles.empty_message_text_style}/>
-            <CustomButton 
-            content={'LLEVAME A LOS PRODUCTOS'} 
-            styleButton={styles.button_shopping_cart_empty_style}
-            styleText={styles.back_button_text_style} 
-            itemButtonDisplay={styles.back_products_button_position}/>
-
-        </div>
+      <TopBarHeader />
+      <div className={styles.empty_shopping_cart_position}>
+        <CustomText
+          content="Aun no has agregado nada a tu carrito."
+          customClassName={styles.empty_message_text_style}
+        />
+        <Link href={`./`} className={styles.link_style}>
+          <button className={styles.button_shopping_cart_empty_style}>
+            <div className={styles.back_products_button_position}>
+              <p className={styles.back_button_text_style}>
+                LLEVAME A LOS PRODUCTOS
+              </p>
+            </div>
+          </button>
+        </Link>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default EmptyShoppingCart
+export default EmptyShoppingCart;
